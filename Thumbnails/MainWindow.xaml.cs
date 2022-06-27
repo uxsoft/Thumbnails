79eb9792594a0a28a58e39e5a07cf7f61113c18d
Jan Dryk <me@uxsoft.cz>
2022-06-27 22:03:10 +0200

    Add project files.

---
+++ Thumbnails/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Thumbnails
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        private void Grid_DragLeave(object sender, DragEventArgs e)
+        {
+            grid.Background = new SolidColorBrush(Colors.White);
+        }
+
+        private void Grid_DragEnter(object sender, DragEventArgs e)
+        {
+            grid.Background = new SolidColorBrush(Colors.Red);
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Link;
+        }
+
+        private void Grid_DropCompleted(UIElement sender, DropCompletedEventArgs e)
+        {
+            Console.WriteLine(e.DropResult);
+        }
+
+        private void border_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                var files = await e.DataView.GetStorageItemsAsync();
+                ThumbnailService.ProcessStorageItems(files);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Thumbnails/ThumbnailService.cs
@@ -0,0 +1,83 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Thumbnails
+{
+    public static class ThumbnailService
+    {
+        public static int Width { get; set; } = 300;
+        public static int Height { get; set; } = 200;
+
+        public static void ProcessStorageItems(IReadOnlyList<IStorageItem> files)
+        {
+            foreach (var file in files)
+                ProcessStorageItem(file);
+        }
+
+        private static void ProcessStorageItem(IStorageItem file)
+        {
+            if (file.IsOfType(StorageItemTypes.Folder))
+                foreach (var item in Directory.GetFiles(file.Path))
+                    ProcessFile(item);
+            else ProcessFile(file.Path);
+        }
+
+        private static void ProcessFile(string path)
+        {
+            try
+            {
+                using var sourceStream = File.OpenRead(path);
+                //using var sourceBitmap = SKBitmap.Decode(sourceStream);
+                using var skiaStream = new SKManagedStream(sourceStream, false);
+                using var sourceImage = SKImage.FromEncodedData(skiaStream);
+                using var sourceBitmap = SKBitmap.FromImage(sourceImage);
+
+
+                var sourceRatio = (double)sourceBitmap.Width / sourceBitmap.Height;
+                var targetRatio = (double)Width / Height;
+
+                int targetHeight;
+                int targetWidth;
+                int x;
+                int y;
+
+                if (sourceRatio > targetRatio)
+                {
+                    targetHeight = Height;
+                    targetWidth = (int)(sourceBitmap.Width * ((double)Height / sourceBitmap.Height));
+                    x = (int)((targetWidth - Width) / 2.0);
+                    y = 0;
+                }
+                else
+                {
+                    targetHeight = (int)(sourceBitmap.Height * ((double)Width / sourceBitmap.Width));
+                    targetWidth = Width;
+                    x = 0;
+                    y = (int)((targetHeight - Height) / 2.0);
+                }
+
+                using var scaledBitmap = sourceBitmap.Resize(new SKImageInfo(targetWidth, targetHeight), SKFilterQuality.High);
+                using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                using var croppedImage = scaledImage.Subset(new SKRectI(x, y, Width + x, Height + y));
+                using SKData data = croppedImage.Encode();
+
+                var outputPath =
+                    Path.Combine(
+                        Path.GetDirectoryName(path),
+                        Path.GetFileNameWithoutExtension(path) + "_thumb.jpg");
+                using var outputStream = File.OpenWrite(outputPath);
+                data.SaveTo(outputStream);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
